607bc8b8f3c699f463d1c3c886324d373248b35e
BBailey1985 <brandon_bailey@hotmail.com>
2023-01-30 20:49:50 -0600

    added components folder and edited files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react"
+import Login from "./components/Login"
+import Header from "./components/Header";
+import CreatePost from "./components/CreatePost";
 
 function App() {
-  return <div>app</div>
+  const [user, setUser] = React.useState('');
+  const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    document.title = user ? `${user}'s Feed` : 'Please Login'
+  }, [user]);
+
+  if (!user) {
+    return <Login setUser={setUser}/>
+  }
+
+  return <>
+    <Header user={user} setUser={setUser} />
+    <CreatePost user={user} setPosts={setPosts} posts={posts}/>
+  </>
 }
 
 export default App
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,32 @@
 import React from "react"
 
-function CreatePost() {
-  return <div>create post</div>
-}
+function CreatePost( { user, setPosts, posts }) {
+  const [content, setContent] = React.useState('');
+  const [image, setImage] = React.useState(null);
 
-export default CreatePost
+  function handleSubmit(event) {
+    event.preventDefault();
+    const post = { content, image, user };
+    const newPosts = [post, ...posts];
+    setPosts(newPosts)
+  }
+
+  return <div>
+    <h1>Create New Post</h1>
+    <form onSubmit={handleSubmit}>
+      <input 
+        type='text'
+        placeholder="Add Post Content"
+        onChange={event => setContent(event.target.value)}
+      />
+      <input
+        type='file'
+        onChange={event => setImage(event.target.files[0])}
+      />
+      <button type='submit'>Submit Post</button>
+    </form>
+  </div>
+
+};
+
+export default CreatePost;
